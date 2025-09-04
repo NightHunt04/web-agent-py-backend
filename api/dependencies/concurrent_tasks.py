7f6d185069fe0b02c366736476df846d86d6eb10
NightHunt04 <jeetbherwani2004@gmail.com>
2025-09-04 18:57:13 +0530

    minor

--- api/dependencies/concurrent_tasks.py
+++ api/dependencies/concurrent_tasks.py
@@ -4,6 +4,9 @@ from ..core.config import settings
 def check_traffic():
     running_tasks = redis.smembers("running-tasks")
 
+    if running_tasks is None:
+        return True
+        
     if len(running_tasks) >= settings.MAX_CONCURRENT_TASKS:
         return False
     return True
